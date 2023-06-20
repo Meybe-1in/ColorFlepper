1ef074d2b719537c610dcd5cb5cf63f86920f559
Dayan <dayanithaclaros@hotmail.com>
2023-06-19 22:00:40 -0600

    end

--- js/app.js
+++ js/app.js
@@ -7,12 +7,15 @@ btn.addEventListener("click", function(){
     // get random number between 0 - 3 colors [3]
     const randomNumber = getRandomNumber();
     console.log(randomNumber);
+
     document.body.style.backgroundColor = colors[randomNumber];
     color.textContent = colors[randomNumber];
 
 });
 
 function getRandomNumber() {
-    return Math.random();
+    return Math.floor(Math.random() * colors.length);
+
     
 }
+
